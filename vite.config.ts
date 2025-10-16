1bb941ea8c261fa47a8d4e41d7024c7292b171a9
Dream <mac@MacBook-Pro.local>
2025-10-16 19:02:43 +0800

    modify some code

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,32 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/",
   build: {
+    outDir: "dist",
     lib: {
       entry: {
         evm: "src/evm/index.ts",
       },
-      name: "zmario",
-      formats: ["es", "cjs"],
+      fileName: (_format, entryName) => `${entryName}.js`,
+      formats: ["cjs", "es"],
+    },
+    rollupOptions: {
+      external: ["viem", "@zlikemario/helper"],
+      output: [
+        {
+          format: "es",
+          preserveModules: true,
+          preserveModulesRoot: "src",
+          entryFileNames: "[name].js",
+          // chunkFileNames: "chunks/[name]-[hash].js",
+        },
+        {
+          format: "cjs",
+          preserveModules: true,
+          preserveModulesRoot: "src",
+          entryFileNames: "[name].cjs",
+          // chunkFileNames: "chunks/[name]-[hash].cjs",
+        },
+      ],
     },
   },
   test: {},
